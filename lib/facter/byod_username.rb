644d44a8477a98bfa81d9da141ec479b379e2784
shadow-kun <shadowreaper@shadowreaper.net>
2018-06-08 16:25:40 +1000

    [Fix] Missing End
    Fixed as missing end and value

--- lib/facter/byod_username.rb
+++ lib/facter/byod_username.rb
@@ -1,11 +1,13 @@
 Factor.add('byod_username') do
    confine :osfamily => :windows
    setcode do
-     begin
-        value = nil
-        Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\CustomFacts') do |regkey|
-        value
-       rescue
+        begin
+          value = nil
+          Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Puppet Labs\CustomFacts') do |regkey|
+            value = regkey['byod_username']
+          end
+          value
+        rescue
           nil
         end
     end
